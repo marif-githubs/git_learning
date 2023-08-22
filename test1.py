526a6832fafc872fe70cf0aad8f30f344f3b243c
marif-githubs <syedmarif.professional@gmail.com>
2023-08-22 14:28:39 +0530

    test1.py added

---
+++ test1.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+print("package imported")
+img=cv2.imread("C:\Users\Dell\OneDrive\Desktop\id photo.jpg")
+cv2.imshow("marif",img)
+print(img.shape)
+resizeimg=cv2.resize(img,(1000,500))
+print(resizeimg.shape)
+cv2.imshow("output",resizeimg)
+cv2.waitKey(50000)
