9b03a449ed7a58a4a6b52c29a7e7db312a4a3126
Syed Marif <syedmarif.professional@gmail.com>
2023-08-22 18:19:35 +0530

    nidda

--- test1.py
+++ test1.py
@@ -1,9 +1,12 @@
+
 import cv2
 import numpy as np
 
 print("package imported")
 img=cv2.imread("C:\Users\Dell\OneDrive\Desktop\id photo.jpg")
 cv2.imshow("marif",img)
+##nigga
+##nigga
 print(img.shape)
 resizeimg=cv2.resize(img,(1000,500))
 print(resizeimg.shape)
